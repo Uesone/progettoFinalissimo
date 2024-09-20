a3c8cf6a126c9b66e50d6642f9895f840c34fa2d
Umberto Amoroso <34135219+Uesone@users.noreply.github.com>
2024-09-20 13:01:23 +0200

    Initial commit

---
+++ src/main/java/UmbertoAmoroso/progettoFinalissimo/ProgettoFinalissimoApplication.java
@@ -0,0 +1,13 @@
+package UmbertoAmoroso.progettoFinalissimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoFinalissimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoFinalissimoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/UmbertoAmoroso/progettoFinalissimo/ProgettoFinalissimoApplicationTests.java
@@ -0,0 +1,13 @@
+package UmbertoAmoroso.progettoFinalissimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoFinalissimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
